2346534ad2681e0a9a7fa0bf3d93982072c8fa74
tatyana12 <tatyana_pol@yahoo.com>
2018-12-30 18:03:32 -0800

    make it better

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const AppRouter = () => (
       <div>
             <Link to="/two/">
               <button type="button"  className="button" >
-               <img src={"./IMG_1342.jpg"} />
+               <img src={"./public/IMG_1342.jpg"} />
              </button>
 
 
